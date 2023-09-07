5f00db2057d815a00c0d8bf18514ed2afdb35f94
PhabloFiap <rm550687@fiap.com.br>
2023-09-07 17:59:00 -0300

    Setado lugares dos frames e feito util para manipulaçao de tamanho

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from tkinter import *  
+import settings
+import util
 
 root = Tk() #instanciando um objeto pois sera uma janela
 root.configure(bg="black") #pintando o fundo
-root.geometry('1220x720') #tamanho da janela
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}') #tamanho da janela
 root.title("Campo minado")
 root.resizable(False, False) #nao redimensionavel
 
 top_frame = Frame (
     root,
     bg= 'red', #vamos mudar mais tarde
-     width=1220,
-     height= 180
+     width=settings.WIDTH,
+     height= util.height_prct(25)
 )
 top_frame.place(x=0, y=0) #mapeando a parte superior
 
 left_frame = Frame (
     root,
     bg='blue',#vamos mudar mais tarde tambem
-    width=305,
-    height=540
+    width=util.width_prct(25),
+    height=util.height_prct(75)
 )
-left_frame.place (x=0, y=180)
+left_frame.place (x=0, y=util.height_prct(25)) #quero iniciar logo apos o quadrado superior
+
+
+center_frame = Frame (
+    root,
+    bg='green', 
+    width=util.width_prct(75),
+    height=util.height_prct(75)
+)
+
+center_frame.place(
+    x=util.width_prct(25),
+    y=util.height_prct(25)
+
+)
 
 root.mainloop()
---
+++ util.py
@@ -0,0 +1,7 @@
+import settings
+
+def height_prct(porcento):
+    return (settings.HEIGHT/100) *porcento
+
+def width_prct(porcento):
+    return(settings.WIDTH / 100)* porcento
