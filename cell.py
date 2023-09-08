0a4b1bd7c5e70ead54b3c3bd190c481eee7916e5
PhabloFiap <rm550687@fiap.com.br>
2023-09-07 21:14:19 -0300

    resolvendo bug dos valores da lista

--- cell.py
+++ cell.py
@@ -1,4 +1,6 @@
 from tkinter import Button
+import random
+import settings
 
 
 class Cell:
@@ -6,17 +8,19 @@ class Cell:
     def __init__(self,i,j, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
-        self.i = i #feito para manipular cada celula especifica
+        self.i = i 
         self.j = j
+
         #criar um objeto ao Cell para lista "All"
         Cell.all.append(self)
 
     def cria_btn_object (self, location):#saber a localizacao do meu botao
         btn = Button(
             location,
-            text=f"{self.i}, {self.j}",
             width=12,
-            height=4
+            height=4,
+            text=f"{self.i}, {self.j}"
+
 
         )
         btn.bind('<Button-1>',self.botao_esquerdo_click)
@@ -24,13 +28,51 @@ class Cell:
         self.cell_btn_object = btn
 
     def botao_esquerdo_click(self, evento):
-        print("clicado com botao esquerdo")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def pega_celula_por_eixo (self, i,j):
+        for cell in Cell.all:
+            if cell.i == i and cell.j == j:
+                return cell
+                
+
+    def show_cell(self):
+        lista_de_celulas = [
+            self.pega_celula_por_eixo(self.i -1, self.j -1),
+            self.pega_celula_por_eixo(self.i -1, self.j),
+            self.pega_celula_por_eixo(self.i -1, self.j +1),
+
+            self.pega_celula_por_eixo(self.i , self.j -1),
+
+            self.pega_celula_por_eixo(self.i +1, self.j -1),
+            self.pega_celula_por_eixo(self.i +1, self.j),
+            self.pega_celula_por_eixo(self.i +1, self.j +1),
+            self.pega_celula_por_eixo(self.i, self.j +1),
+        ]
+        lista_de_celulas = [i for i in lista_de_celulas]
+        print (lista_de_celulas)
+      
+
+
+    def show_mine(self):
+        #logica pra quando jogador clica na mina
+        self.cell_btn_object.config(bg='blue')
+
     def botao_direito_click(self, evento):
-        print("clicado com botao direito")
+        print (evento)
+        
      
     @staticmethod
     def randomize_mines():
-        pass
+    
+        celulas_pegada = random.sample(
+            Cell.all, settings.MINES_COUNT
+        )
+        for i in celulas_pegada:
+            i.is_mine = True
 
     def __repr__(self):
         return f"Cell({self.i}, {self.j})"
